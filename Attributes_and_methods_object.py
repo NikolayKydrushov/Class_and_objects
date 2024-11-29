1b6f28ea54e73bdc098a4dd3142d68c67aea6631
Kolya <nnikolay2909@gmail.com>
2024-11-29 16:56:51 +0300

    Итоговый вариант задачи "Специальные методы классов"

--- Attributes_and_methods_object.py
+++ Attributes_and_methods_object.py
@@ -1,7 +1,17 @@
+from tkinter.font import names
+
+
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        #print (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
     def go_to(self, new_floor):
         for i in range(1, new_floor+1):
@@ -12,10 +22,17 @@ class House:
                 print(i)
 
 
-h1 = House('ЖК Горский', 18)
+#h1 = House('ЖК Горский', 18)
+#h2 = House('Домик в деревне', 2)
+#h1.go_to(5)
+#h2.go_to(10)
 
-h2 = House('Домик в деревне', 2)
 
-h1.go_to(5)
-
-h2.go_to(10)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+# __str__
+print(h1)
+print(h2)
+# __len__
+print(len(h1))
+print(len(h2))
